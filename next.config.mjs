06c333b817a46a89adb9abbba14a8bf51274d77f
Md Towfiqur Rahman <140777602+towfiqurv360@users.noreply.github.com>
2026-09-20 16:18:45 +0600

    fix: implement Next.js rewrites to resolve cross-domain cookie block and 401 unauthorized login issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://b13-a10-recopehub-server.onrender.com/api/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
